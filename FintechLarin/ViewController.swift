971dc3f5c776867cfa49a628d438bb430524ad61
ivanlardis <ivan@lardis.ru>
2018-09-23 10:16:00 +0300

    Initial Commit

---
+++ FintechLarin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FintechLarin
+//
+//  Created by Иван Lardis on 23.09.18.
+//  Copyright © 2018 ivan larin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
