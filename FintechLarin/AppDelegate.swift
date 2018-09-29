59f3f9f1a1e0178decca40dc44cc796c510ce855
ivanlardis <ivan@lardis.ru>
2018-09-29 20:03:00 +0300

    - чистка проекта перед 2 заданием

--- FintechLarin/AppDelegate.swift
+++ FintechLarin/AppDelegate.swift
@@ -13,39 +13,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var lastState = "Not running"
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Logger.setDebugMode(isDebug: true)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        showState(functionName: #function)
+
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        showState(functionName: #function)
+   
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        showState(functionName: #function)
+
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        showState(functionName: #function)
+
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        showState(functionName: #function)
-    }
 
-    func showState(functionName: String) {
-        let curState = UIApplication.shared.applicationState.name()
-        Logger.d(message: "Application moved from \(lastState) to \(curState): \(functionName).")
-        lastState = curState
     }
-    
 }
 
 
--- FintechLarin/ViewController.swift
+++ FintechLarin/ViewController.swift
@@ -12,47 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        printMethod(functionName: #function)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        printMethod(functionName: #function)
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        printMethod(functionName: #function)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        printMethod(functionName: #function)
-    }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        printMethod(functionName: #function)
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        printMethod(functionName: #function)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        printMethod(functionName: #function)
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        printMethod(functionName: #function)
-    }
-
-    func printMethod(functionName :String){
-        Logger.d(message: "ViewController: \(functionName).")
-    }
-    
 }
 
